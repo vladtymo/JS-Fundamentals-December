f5d8df646b17b3a3e04af9fe744b7dac2f1e75f7
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-01-23 16:35:35 +0200

    AJAX example

--- 15 Node.js/src/express.js
+++ 15 Node.js/src/express.js
@@ -1,6 +1,7 @@
 // ---------- server logic ----------
 
 const fs = require('fs');
+const cors = require('cors');
 
 // connect express module
 const express = require('express'),
@@ -8,6 +9,10 @@ const express = require('express'),
 
 const hostname = 'localhost';
 const port = 4400;
+
+app.use(
+    cors({ origin: "*" })
+);
 
 // GET: ~/api/contacts
 app.get("/api/contacts", (req, res) => {
